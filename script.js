c43febdf9822a588a72f189ab22be33240cb4b3a
MABBenton <mabbenton7@gmail.com>
2023-03-27 12:58:32 -0600

    added traffic-chart data and variable

--- script.js
+++ script.js
@@ -20,3 +20,16 @@ alertBanner.innerHTML =
   for (let i = 0; i < activityArrow.length; i++) {
     activityArrow[i].insertAdjacentHTML('beforeend',`<div class='carrot'>></div>`);
   }
+
+  // Chart Widgets
+
+  const trafficCanvas = document.getElementById('traffic-chart');
+
+  let trafficData = {
+    labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31"],
+    datasets: [{
+      data: [0, 250, 500, 750, 1000, 1250, 1500, 1750, 2000, 2250, 2500],
+      backgroundColor: 'rgb(127, 102, 195)',
+      borderWidth: 1,
+    }]
+  };
