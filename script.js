cb32a42998f3d0fc6f00146bf31ba7bb5d045ce8
MABBenton <mabbenton7@gmail.com>
2023-04-04 20:58:26 -0700

    added message section event listener

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//alert banner
+// alert banner
 const alertBanner = document.getElementById('alert');
 
 alertBanner.innerHTML =
@@ -20,3 +20,20 @@ const activityArrow = document.getElementsByClassName('activity-container');
 for (let i = 0; i < activityArrow.length; i++) {
   activityArrow[i].insertAdjacentHTML('beforeend',`<div class='carrot'>></div>`);
 }
+
+// message section
+const user = document.getElementById('userField');
+const message = document.getElementById('messageField');
+const send = document.getElementById('send');
+
+send.addEventListener('click', () => {
+  if ( user.value === '' && message.value === '') {
+    alert("Both fields must be filled in before sending.");
+  } else if ( user.value === '' ) {
+    alert("The user field must be filled in before sending.");
+  } else if ( message.value === '' ) {
+    alert("The message field must be filled in before sending.");
+  } else {
+    alert(`Your message was sent to: ${user.value}`);
+  }
+});
